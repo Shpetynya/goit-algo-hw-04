6099e6ed6b20da1b5920a481ef2ffa1f7152fc8c
Shpetynya <olga.shpetnaja@gmail.com>
2024-08-20 20:13:20 +0300

    fixed

--- task_2.py
+++ task_2.py
@@ -1,15 +1,17 @@
 import re
+from typing import Callable
 
-def generator_numbers(text):
+
+def generator_numbers(text: str):
     # Використовуємо регулярний вираз для пошуку всіх чисел у тексті
-    for match in re.findall(r'\b\d+\.\d+|\b\d+', text):
-        # Генеруємо числа как float
+    for match in re.findall(r' \d+\.\d+ | \d+ ', text):
+        # Генеруємо числа як float
         yield float(match)
 
 
-def sum_profit(text):
-    # Використовуємо функцію generator_numbers для отримання всіх чисел з тексту
-    return sum(generator_numbers(text))
+def sum_profit(text: str, func: Callable) -> float:
+    # Використовуємо передану функцію-генератор для отримання всіх чисел з тексту
+    return sum(func(text))
 
 text = "The company reported profits of 200.50, 1500.75 and 800.25 in the first quarter."
-print(f"Total profit: {sum_profit(text)}")
+print(f"Total profit: {sum_profit(text, generator_numbers)}")
